3d5a4f58f13a8d9501b4545bd896f5347091004d
gruoner <gruoner@github>
2025-06-15 16:10:12 +0200

    Services zum Starten und Stoppen bei Hibernate & Co. angepasst

---
+++ osync-sleep.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+typeset USERS=""
+for u in $(who -s | cut -d" " -f1 | sort -u)
+do
+  if ! grep ^$u /etc/passwd > /dev/null 2>&1; then
+#        echo "user $u not found in /etc/passwd"
+	USERS=$USERS" "$u
+#  else
+#        echo "found user $u in /etc/passwd"
+  fi
+done
+
+case "$1" in
+    suspend|hibernate)
+	    for u in $USERS
+	    do
+		systemctl --machine=$u@ --user stop osync-srv@base-profile.conf
+	    done
+        exit 0
+        ;;
+    resume)
+	    for u in $USERS
+	    do
+		 systemctl --machine=$u@ --user start osync-srv@base-profile.conf
+	    done
+        exit 0
+        ;;
+    *)
+        exit 1
+esac
--- start-osync.sh
+++ start-osync.sh
@@ -3,7 +3,15 @@
 if ! grep ^$(whoami) /etc/passwd > /dev/null 2>&1; then
 	echo "running as an user not found in /etc/passwd"
 	echo "thus starting osync as user $(whoami)"
-	systemctl --user start osync-srv@base-profile.conf
+	if systemctl --user status osync-srv@base-profile.conf.service > /dev/null 2>&1
+	then
+	  echo "base-profile service still running"
+	else
+ 	  echo "first download base-profile"
+	  /usr/local/bin/osync.sh /etc/osync/base-profile.conf --summary --stats --non-interactive --sync-type="target2initiator"
+ 	  echo "then starting base-profile"
+	  systemctl --user start osync-srv@base-profile.conf
+	fi
 else
 	echo "found user $(whoami) in /etc/passwd"
 	echo "thus not starting osync"
