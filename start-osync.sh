3a523d5a6fe54e6b238b3058a32e60e39a0b49ea
gruoner <gruoner@github>
2025-07-12 11:03:51 +0200

    bugfix

--- start-osync.sh
+++ start-osync.sh
@@ -7,8 +7,13 @@ if ! grep ^$(whoami) /etc/passwd > /dev/null 2>&1; then
 	then
 	  echo "base-profile service still running"
 	else
- 	  echo "first download base-profile"
-	  /usr/local/bin/osync.sh /etc/osync/base-profile.conf --summary --stats --non-interactive --sync-type="target2initiator"
+	  if [ -f $(eval echo ~$(whoami))/osync.base-profile.log ]
+          then
+                echo "osync had initialy synced the homedir -- supressing backsync"
+          else
+                echo "first download base-profile"
+                /usr/local/bin/osync.sh /etc/osync/base-profile.conf --summary --stats --non-interactive --sync-type="target2initiator"
+          fi
  	  echo "then starting base-profile"
 	  systemctl --user start osync-srv@base-profile.conf
 	fi
