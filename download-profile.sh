40eaf954478c585c6026e71cc9662ffb47630443
gruoner <gruoner@github>
2025-07-16 21:59:19 +0200

    Bugfixes und fehlende Dateien

--- download-profile.sh
+++ download-profile.sh
@@ -4,3 +4,10 @@
 
 /usr/local/bin/osync.sh /etc/osync/extended-profile.conf --summary --stats --non-interactive --sync-type="target2initiator"
 
+echo "=================================="
+echo "ACHTUNG!! Du wirst nun ausgeloggt, damit dein heruntergeladenes Profil aktiv werden kann!"
+
+read
+
+gnome-session-quit --logout
+
--- start-osync.sh
+++ start-osync.sh
@@ -15,7 +15,7 @@ if ! grep ^$(whoami) /etc/passwd > /dev/null 2>&1; then
                 /usr/local/bin/osync.sh /etc/osync/base-profile.conf --summary --stats --non-interactive --sync-type="target2initiator"
           fi
  	  echo "then starting base-profile"
-	  systemctl --user start osync-srv@base-profile.conf
+#	  systemctl --user start osync-srv@base-profile.conf
 	fi
 else
 	echo "found user $(whoami) in /etc/passwd"
